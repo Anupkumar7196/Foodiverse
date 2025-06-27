1b1ea0d2579cf63755ac60750f1184b760d51f22
Anup kumar <anupk7196@gmail.com>
2025-06-27 18:24:56 +0530

    update

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -8,3 +8,14 @@ const appStore = configureStore({
     },
 });
 export default appStore;
+
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import rootReducer from "./reducers";
+
+// const store = configureStore({
+//   reducer: rootReducer,
+//   devTools: process.env.NODE_ENV !== "production",
+// });
+
+// export default appStore;
